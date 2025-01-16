637bd779509962c54d4bbb51be25944a1c3fe913
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-01-16 20:42:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
@@ -1,20 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+    int xorAllNums(vector<int> &nums1, vector<int> &nums2)
+    {
         int res = 0;
-        int xor1 = 0;
-        int xor2 = 0;
-        for(auto val : nums1){
-           xor1^=val;
-        }
-        for(auto val2 : nums2){
-            xor2^=val2;
-        }
-        if(nums2.size()%2!=0){
-            res^=xor1;
-        }
-        if(nums1.size()%2!=0){
-            res^=xor2;
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            for (int j = 0; j < nums2.size(); j++)
+            {
+                if (nums1[i] == nums2[j])
+                    continue;
+                int x = nums1[i] ^ nums2[j];
+                res = res ^ x;
+            }
         }
         return res;
     }
