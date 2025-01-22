c169cb8af54f047d9c5104e4de3e904290857dc2
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-01-22 22:34:48 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,5 +1,19 @@
 
-// 
+/*
+    class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> st;
+        
+        for(int i=0; i<nums.size();i++){
+             st.insert(nums[i]);
+        }
+        vector<int>temp(st.begin(),st.end());
+        nums = temp;
+        return nums.size();
+    }
+}; */
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
