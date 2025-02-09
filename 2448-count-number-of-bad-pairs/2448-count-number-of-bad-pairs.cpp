1bf90ecbc22339c318292372c3e0f4d7c0d8b9ed
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-02-09 23:30:02 +0530

    Time: 86 ms (17.02%), Space: 92.6 MB (19.3%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        int n = nums.size();
-        long long totalPairs = (long long)n * (n - 1) / 2; // Total pairs in the array
-
-        unordered_map<int, long long> freq; // To store frequency of nums[i] - i
-        long long goodPairs = 0;
-
-        for (int i = 0; i < n; i++) {
-            int key = nums[i] - i;
-
-            // If key already exists in the map, it means there are good pairs with this key
-            if (freq.find(key) != freq.end()) {
-                goodPairs += freq[key]; // Add all pairs formed by current index
+       long long int n= nums.size();
+       long long int size = n *(n-1)/2;
+       unordered_map<int,long long> freq;
+       long long goodPair = 0;
+       for(int i=0; i<n; i++){
+            int temp = nums[i]-i;
+            if(freq.find(temp)!=freq.end()){
+                goodPair+=freq[temp];
             }
-
-            // Update frequency of the current key
-            freq[key]++;
-        }
-
-        // Calculate bad pairs as total pairs - good pairs
-        return totalPairs - goodPairs;
+            freq[temp]++;
+       }
+       return size - goodPair;
     }
 };
+/* int n = nums.size();
+        long long int count = 0;
+        unordered_map<int,int> map;
+        for(int i = 0 ; i<n ; i++){
+            for(int j=0; j<n; j++){
+                
+                if(i<j && nums[i]-i != nums[j]-j){
+                    map[i]++;
+                }
+            }
+        } // shows tle  56/65  need to optimize
+        for(auto pair : map){
+            count+=pair.second;
+        }
+        return count;
+    } 
+
+
+     int n = nums.size();
+        long long int count = 0;
+        for(int i = 0 ; i<n ; i++){
+            for(int j=0; j<n; j++){
+                
+                if(i<j && nums[i]-i != nums[j]-j){
+                    count++;
+                }
+            }
+        } // shows tle  56/65  need to optimize
+        return count;
+    } */
+
+
+    
