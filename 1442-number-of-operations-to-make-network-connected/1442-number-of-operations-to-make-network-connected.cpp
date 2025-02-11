d60b028a6c29b2136e9de0d20d90bef79be6b6b6
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-02-11 21:16:10 +0530

    Time: 51 ms (11.26%), Space: 57.6 MB (8.36%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+    void dfs(int node, vector<vector<int>> &adj, vector<int>& vis) {
         vis[node] = 1;
         for (auto it : adj[node]) {
             if (!vis[it]) {
@@ -11,31 +11,24 @@ private:
 
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        // Edge case: Not enough cables
-        if (connections.size() < n - 1)
-            return -1;
-
-        // Build the adjacency list
         vector<vector<int>> adj(n);
+        vector<int> vis(n, 0);
+        // edge case
+        if (connections.size() < n - 1) return -1;
         for (auto it : connections) {
             int val1 = it[0];
             int val2 = it[1];
             adj[val1].push_back(val2);
             adj[val2].push_back(val1);
         }
-
-        // Count the number of connected components
-        vector<int> vis(n, 0);
-        int connectedComponents = 0;
-
+        
+        int minCount = 0;
         for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                connectedComponents++;
+            if (!vis[i]){
+                minCount++;
                 dfs(i, adj, vis);
             }
         }
-
-        // Number of operations needed to connect all components
-        return connectedComponents - 1;
+        return minCount-1;
     }
 };
