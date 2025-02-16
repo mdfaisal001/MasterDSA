0aceaf34f6806b7b7142a38b515be1f710a2b10e
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-02-16 14:44:35 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int> map;
+      int n = nums.size();
+      int low = 1;
+      int high = n-2;
+      if(n==1) return nums[0];
+      if(nums[0]!=nums[1]) return nums[0];
+      if(nums[n-1]!= nums[n-2]) return nums[n-1] ; // edge cases
+      while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(nums[mid]!=nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+        if( (mid%2 == 0 && nums[mid]==nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1]) ){
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+      }
+      return 0;
+    }
+};
+
+/*  unordered_map<int,int> map;
         for(int i =0 ; i<nums.size(); i++){
             map[nums[i]]++;
         }
         for(auto pair : map){
             if (pair.second < 2) return pair.first;
         }
-        return 0;
-    }
-};
+        return 0;*/
