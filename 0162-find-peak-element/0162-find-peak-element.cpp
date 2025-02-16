67c90c3d0189f14a76a64f2bbe2c5c657c1438d9
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-02-16 14:21:59 +0530

    Time: 0 ms (100%), Space: 12.5 MB (66.28%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
         int low = 0;
-        int high = nums.size()-1;
+        int high = n-1;
         while(low < high){
             int mid = low + (high - low) / 2;
             if(nums[mid] > nums[mid + 1]) high = mid ;
