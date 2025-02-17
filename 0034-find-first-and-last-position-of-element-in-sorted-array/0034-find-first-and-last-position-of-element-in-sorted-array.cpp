297d8fc5a960613d4e3651643992e669efd23066
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-02-17 21:57:16 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -19,23 +19,27 @@ public:
             }
         }
         if(result != -1 && nums[result]==target) vec[0] = result;
-        int start = 0;
-        int end = nums.size()-1;
-        int last = -1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(nums[mid]==target){
-                last = mid;
-                start = mid+1;
-            }
-            else if(nums[mid] > target){
-                end = mid-1;
-            }
-            else{
-                 start = mid + 1;
-            }
-        }
-        vec[1] = last;
+        
+        if(result !=-1){
+                    int start = 0;
+                    int end = nums.size()-1;
+                    int last = -1;
+                    while(start <= end){
+                        int mid = start + (end - start) / 2;
+                        if(nums[mid]==target){
+                            last = mid;
+                            start = mid+1;
+                        }
+                        else if(nums[mid] > target){
+                            end = mid-1;
+                        }
+                        else{
+                            start = mid + 1;
+                        }
+                    }
+                    vec[1] = last;
+                    }
+                
         return vec;
     }
 };
