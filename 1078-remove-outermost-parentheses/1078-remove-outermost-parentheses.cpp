d5ac5d4b62ef79d8e4f431c4eddbc1d7835ab025
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-02 16:53:52 +0530

    Time: 0 ms (100%), Space: 9 MB (25.94%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count = 0;
+        string result = "";
+        for(char c : s){
+            if(c == '('){
+                if(count > 0) result+=c;
+                count++;
+            }
+            if(c == ')'){
+                count--;
+                if(count>0) result+=c;
+            }
+        }
+        return result;
+    }
+};
