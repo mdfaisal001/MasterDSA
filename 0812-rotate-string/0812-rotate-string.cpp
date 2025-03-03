06445d347246897cd8e6dbc8be0790e069d4636c
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-03 21:34:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.53%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.size();
-        for(int i=0; i<n; i++){
+       if(s.size() == goal.size() && (s+s).find(goal) != string :: npos) return true;
+       return false;
+ }
+};
+/* for(int i=0; i<n; i++){
             char first = s[0];
             for(int j=0; j<n-1;j++){
                 s[j] = s[j+1];
@@ -11,5 +14,18 @@ public:
         if(s == goal) return true;
     }
     return false;
- }
-};
+
+     int n = s.size();
+        string search = "";
+        int i;
+       for(i=0; i<n;i++){
+        if(s[i]!=goal[0]){
+            search+=s[i];
+        }
+        else break;
+       }
+       string word = goal.substr(i,n-i);
+       if(word.find(search)!= string::npos) return true;
+       return false;
+    
+    */
