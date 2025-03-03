b5e523d5ed92d5b68658a1ad4f7ec10dd8437c1d
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-03 21:11:07 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.53%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            char first = s[0];
+            for(int j=0; j<n-1;j++){
+                s[j] = s[j+1];
+            }
+            s[n-1] = first;
+        if(s == goal) return true;
+    }
+    return false;
+ }
+};
