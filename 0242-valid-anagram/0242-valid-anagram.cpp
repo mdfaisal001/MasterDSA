1b8ffe285f371c03f4327687cd84b6d212ec5954
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-05 22:31:57 +0530

    Time: 7 ms (22.38%), Space: 9.6 MB (74.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -7,10 +7,21 @@ public:
         }
         for(auto it : t){
             mp[it]--;
-        }
+        }  
        for(auto pair : mp){
             if(pair.second != 0) return false;
        }
        return true;
     }
 };
+
+/* by skipping above two loops just for loop through n because the
+        length will be equeal then increase and decrease freq in single loop before this length not equeal
+        return false
+           if (s.length() != t.length()) return false;
+        int n = s.length();
+        unordered_map<char, int> counts;
+        for (int i = 0; i < n; i++) {
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }*/
