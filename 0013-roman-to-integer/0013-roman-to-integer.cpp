c6116c57b7d596b36f0e6cff4bed715ec1c9473f
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-08 17:34:45 +0530

    Time: 9 ms (33.9%), Space: 13.5 MB (19.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -10,11 +10,11 @@ public:
         {'D', 500},
         {'M', 1000}
     };
-       int num = 0;
+       int num =0;
        int n = s.size();
       
        for(int i=0;i<n; i++){
-            if(i < n-1 && mp[s[i]]  < mp[s[i+1]]){
+            if( i < n-1 && mp[s[i]]  < mp[s[i+1]]){
                 num = num - mp[s[i]];
             }
             else{
@@ -24,3 +24,30 @@ public:
        return num;
     }
 };
+
+/*  int romanToInt(string s) {
+    map<char, int> mp = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+       int num = mp[s[0]];
+       int n = s.size();
+      
+       for(int i=1;i<n; i++){
+            if(i== n -1){
+                num += mp[s[n]];
+            }
+            if(mp[s[i]]  < mp[s[i+1]]){
+                num = num - mp[s[i]];
+            }
+            else{
+                num = num + mp[s[i]];
+            }
+       } 
+       return num;
+    } */
