e1b86330ec90c70b08d8f5dead2fcff7bb5e2ae2
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-11 22:13:11 +0530

    Time: 434 ms (7.51%), Space: 29.1 MB (20.32%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,35 +1,58 @@
 class Solution {
-private:
-    void frequency(map<char, int> &mp, const string &substring) {
-        for (char c : substring) {
-            mp[c]++;
-        }
-    }
-
 public:
     int beautySum(string s) {
+        int n = s.size();
         int sum = 0;
-
-        // Generate all substrings and calculate beauty
-        for (int i = 0; i < s.size(); i++) {
-            map<char, int> mp; // Fresh map for each starting index
-
-            for (int j = i; j < s.size(); j++) {
-                mp[s[j]]++; // Update frequency map dynamically
-
-                // Find min and max frequency elements in the map
-                auto minimum = min_element(mp.begin(), mp.end(), 
-                    [](const auto &a, const auto &b) { return a.second < b.second; });
-                auto maximum = max_element(mp.begin(), mp.end(), 
-                    [](const auto &a, const auto &b) { return a.second < b.second; });
-
-                // Calculate beauty difference
-                int maxi = maximum->second;
-                int mini = minimum->second;
-                sum += (maxi - mini);
+        for(int i=0; i<n; i++){
+            map<char,int> mp;
+            for(int j = i ; j<n; j++){
+                mp[s[j]]++; // for each possible substr calculating the freq;
+                
+                auto mini = min_element(mp.begin(),mp.end(),
+                [](const auto &a , const auto &b) {return a.second < b.second ;});
+                auto maxi = max_element(mp.begin(), mp.end(),[]
+                (const auto &a, const auto &b) {return a.second < b.second ;});
+                int maximum = maxi->second;
+                int minimum = mini->second;
+                sum +=(maximum - minimum);
             }
         }
-
-        return sum;
+       return sum;
     }
 };
+/*class Solution {
+private:
+    void frequency(map<char,int> &mp , string &substring){
+            for(char c : substring){
+                mp[c]++;    
+        }
+    }
+public:
+    int beautySum(string s) {
+        vector<string>substring;
+        int sum = 0;
+        for(int i=0; i<s.size(); i++){
+            for(int j=0; j< s.size();j++){
+                string temp="";
+                for(int k=i; k<=j;k++){
+                     temp +=s[k];
+                }
+                substring.push_back(temp);
+            }
+        }
+        map<char,int>mp;
+        for(auto substr : substring){
+            frequency(mp,substr);
+            auto minimum = min_element(mp.begin(), mp.end(), 
+    [](const auto &a, const auto &b) { return a.second < b.second; });
+            auto maximum = max_element(mp.begin(), mp.end(), 
+    [](const auto &a, const auto &b) { return a.second < b.second; });
+            int maxi = maximum->second;
+            int mini = minimum->second;
+            int total = maxi - mini;
+            sum+=total;
+            mp.clear();
+        }
+        return sum;
+    }
+};*/
