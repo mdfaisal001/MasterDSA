1e008d70779723ef053f4e119a47c3d77f3c25fb
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-03-17 21:17:15 +0530

    Time: 6 ms (90.75%), Space: 11.7 MB (96.44%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,6 +11,7 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
+        if(head == NULL) return false;
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
