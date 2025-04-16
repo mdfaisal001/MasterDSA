3875695d3df41c21e79a814ce9822eedd97ac4da
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-04-16 09:10:46 +0530

    Time: 4 ms (22.26%), Space: 11.4 MB (26.76%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -20,3 +20,37 @@ public:
       return result;
     }
 };
+
+/*class Solution {
+private:
+    void generate(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& result, set<vector<int>>& seen) {
+        if (index == nums.size()) {
+            // Use set to avoid duplicates
+            if (seen.find(temp) == seen.end()) {
+                result.push_back(temp);
+                seen.insert(temp);
+            }
+            return;
+        }
+
+        // Not pick current element
+        generate(index + 1, nums, temp, result, seen);
+
+        // Pick current element
+        temp.push_back(nums[index]);
+        generate(index + 1, nums, temp, result, seen);
+        temp.pop_back();  // backtrack
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); // Sorting helps ensure duplicates are adjacent
+        vector<vector<int>> result;
+        vector<int> temp;
+        set<vector<int>> seen; // to avoid duplicate subsets
+
+        generate(0, nums, temp, result, seen);
+        return result;
+    }
+}; // brute 2^n logn n
+*/ 
