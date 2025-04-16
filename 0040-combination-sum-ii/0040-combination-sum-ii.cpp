1dfcb265eeee802cffe95a43aed66ca68657594d
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-04-16 09:10:19 +0530

    Time: 3 ms (42.52%), Space: 15.5 MB (16.23%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -12,7 +12,7 @@ private:
       
             if(i > index && combination[i] == combination[i-1]) continue;
 
-                 if(combination[i] >target)  break  ;// if the conditon was broke on the first call theres no meaning to check other calls its definitely have larger elements bcz its sorted
+                 if(combination[i] > target)  break  ;// if the conditon was broke on the first call theres no meaning to check other calls its definitely have larger elements bcz its sorted
                      temp.push_back(combination[i]);
                      getCombination(i + 1, target - combination[i], temp, result, combination);
                      temp.pop_back();
@@ -30,3 +30,37 @@ public:
         return result;
     }
 };
+
+/*class Solution {
+private:
+    void generate(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& result, set<vector<int>>& seen) {
+        if (index == nums.size()) {
+            // Use set to avoid duplicates
+            if (seen.find(temp) == seen.end()) {
+                result.push_back(temp);
+                seen.insert(temp);
+            }
+            return;
+        }
+
+        // Not pick current element
+        generate(index + 1, nums, temp, result, seen);
+
+        // Pick current element
+        temp.push_back(nums[index]);
+        generate(index + 1, nums, temp, result, seen);
+        temp.pop_back();  // backtrack
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); // Sorting helps ensure duplicates are adjacent
+        vector<vector<int>> result;
+        vector<int> temp;
+        set<vector<int>> seen; // to avoid duplicate subsets
+
+        generate(0, nums, temp, result, seen);
+        return result;
+    }
+}; // brute 2^n logn n
+*/ 
