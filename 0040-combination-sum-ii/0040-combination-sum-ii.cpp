69501f23ef51d5017a304cafce3d77503996e975
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-04-16 07:45:16 +0530

    Time: 4 ms (27.3%), Space: 15.5 MB (17.6%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    void getCombination(int index, int target, vector<int> temp, vector<vector<int>> &result, vector<int>& combination) {  
+        if (target == 0) {
+           result.push_back(temp);
+           return;
+        }
+
+        
+        for(int i = index; i < combination.size();i++){  // to explore multiple option
+
+      
+            if(i > index && combination[i] == combination[i-1]) continue;
+             if(combination[i] > target) break;
+
+             temp.push_back(combination[i]);
+             getCombination(i + 1, target - combination[i], temp, result, combination);
+             temp.pop_back();
+        
+      }
+ }
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); // for consistency
+        vector<int> temp;
+        vector<vector<int>> result;
+
+        getCombination(0, target, temp, result, candidates);
+
+        return result;
+    }
+};
