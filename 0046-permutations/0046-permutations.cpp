b7fc9cbafa91c7435290030fe9bc0c2e273314bf
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-04-18 18:17:44 +0530

    Time: 7 ms (8.23%), Space: 12.9 MB (8.82%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,7 +1,37 @@
 class Solution {
 private:
+    void permutation(int index, vector<int> &nums, vector<vector<int>> &result ,vector<int>map,vector<int> temp){
+       if(index == nums.size()){
+            result.push_back(temp);
+            return;
+       }
+       for(int i=0; i<nums.size(); i++){
+            if(!map[i]){
+                map[i] = 1;
+                temp.push_back(nums[i]);
+                permutation(index+1,nums,result,map,temp);
+                map[i] = 0;
+                temp.pop_back();
+            }
+       }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+       vector<int>map(nums.size(),0);
+       vector<vector<int>> result;
+       vector<int> temp;
+       permutation(0,nums,result,map,temp);
+       return result;
+    }
+};
+
+
+
+
+/*class Solution {
+private:
     void findPermutation(int index, vector<int> nums, vector<vector<int>> &result){
-        if(index == nums.size() -1) {
+        if(index == nums.size() -1) { // by adding -1 we can decrease the one level of recursion , its further efficient
             result.push_back(nums);
             return;
         } // base case
@@ -17,4 +47,4 @@ public:
     findPermutation(0,nums,result);
     return result;   
     }
-};
+};*/
