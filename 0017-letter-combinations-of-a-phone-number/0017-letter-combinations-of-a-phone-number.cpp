292d09a81ba71903f16071a46b1e713456821d27
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-04-18 21:02:32 +0530

    Time: 1 ms (20.55%), Space: 9.2 MB (57.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
+    
 private:
+        unordered_map<char, string> mp= {
+                {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+    };
+
+
     void findCombination(int index , string combination, vector<string> &result, string &digits){
         if(index == digits.size()){
             result.push_back(combination);
         }
-        unordered_map<char, string> mp= {
-                {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-};
+        
         string temp = mp[digits[index]];
         for(char ch : temp){
             combination.push_back(ch);
