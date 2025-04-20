0b2fa1173fa3dbf2541ad886f18fe6f123da5119
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-04-20 12:11:31 +0530

    Time: 149 ms (10.86%), Space: 149.1 MB (8.38%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    bool findPalindrome(string substring , int start , int end){
+        while(start < end){
+            if(substring[start]!=substring[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    void findPartition(int start, string s, vector<string> temp, vector<vector<string>> &result){
+        if(start == s.size()){
+            result.push_back(temp);
+            return;
+        }
+
+        for(int end=start; end<s.size(); end++){
+            if(findPalindrome(s,start,end)){
+                temp.push_back(s.substr(start, end-start +1));
+                findPartition(end+1,s,temp,result);
+                temp.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string> temp;
+        vector<vector<string>>result;
+        int start = 0;
+        findPartition(start,s,temp,result);
+        return result;
+    }
+};
