b4a8994ae5884f33bfecb59f31b0f2c245057f80
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-05-01 09:39:11 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.35%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+  bool isPowerOfTwo(int n) {
+    if (n == 1) return true;         // Base case: 2^0 = 1
+    if (n == 0 || n % 2 != 0) return false; // Reject zero or odd numbers
+    return isPowerOfTwo(n / 2);      // Recur by dividing n by 2
+}
+
+};
