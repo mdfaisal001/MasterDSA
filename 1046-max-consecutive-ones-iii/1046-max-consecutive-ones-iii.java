64f9dd20010df453cc37b9dd3dd7d3eb3bfb9f2f
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-06-25 23:27:05 +0530

    Time: 3 ms (93.56%), Space: 46.9 MB (22.01%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,4 +1,5 @@
 class Solution {
+    //optimal
     public int longestOnes(int[] nums, int k) {
         int n = nums.length , l= 0 ,r =  0 , maxLen = 0, zeroes = 0;
         while(r < n){
@@ -15,3 +16,39 @@ class Solution {
         return maxLen;
     }
 }
+
+/* brute 
+int maxLen = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int zeroes = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == 0) zeroes++;
+
+                if (zeroes > k) break;
+
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+
+        return maxLen;
+        
+    // Better 
+    int n= nums.size() , l= 0 ,r =  0 , maxLen = 0, zeroes = 0;
+        
+        while(r < n){
+            if(nums[r] == 0) zeroes++;
+            while(zeroes > k){
+                if(nums[l] == 0) zeroes--;
+                l++;
+            }
+            if(zeroes <=k){
+                maxLen = max(maxLen,r-l+1);
+                r++;
+            }
+        }
+        return maxLen;
+        
+        
+         */
