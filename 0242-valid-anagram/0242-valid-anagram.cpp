605132d6453576e906c1a9090cbdbaeb912276b6
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-05 21:59:31 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mp;
-        for(auto ch : s){
-            mp[ch]++;
-        }
-        for(auto it : t){
-            mp[it]--;
-        }  
-       for(auto pair : mp){
-            if(pair.second != 0) return false;
+        unordered_map<char,int>mp;
+        if (s.length() != t.length())
+            return false;
+       for(char letters : t){
+           mp[letters]++;
+       } 
+       for(auto character : s){
+            if(mp.find(character) != mp.end()){
+                mp[character]--;
+                if(mp[character] == 0) mp.erase(character);
+            }
        }
-       return true;
+       return mp.empty();
     }
 };
-
-/* by skipping above two loops just for loop through n because the
-        length will be equeal then increase and decrease freq in single loop before this length not equeal
-        return false
-           if (s.length() != t.length()) return false;
-        int n = s.length();
-        unordered_map<char, int> counts;
-        for (int i = 0; i < n; i++) {
-            counts[s[i]]++;
-            counts[t[i]]--;
-        }*/
