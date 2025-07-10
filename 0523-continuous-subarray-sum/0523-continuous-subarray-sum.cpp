1406108195cdcd78421f76823a0cbfb9a4c840f9
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-10 12:57:48 +0530

    Time: 76 ms (63.35%), Space: 146.4 MB (25.78%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> hash;
-        hash[0] = -1; // base case: subarray from beginning
-        int sum = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            int mod = (k == 0) ? sum : sum % k;
-
-            if (hash.find(mod) != hash.end()) {
-                if (i - hash[mod] >= 2) return true;
-            } else {
-                hash[mod] = i;
+        unordered_map<int,int>hash;
+        hash[0] = -1;
+        int n = nums.size(), sum = 0;int mod = -1;
+        for(int i=0; i<n ; i++){
+            sum+=nums[i];
+            mod = (k==0)? sum : sum%k; 
+            if(hash.find(mod) != hash.end()){
+                int len = i - hash[mod];
+                if(len >=2) return true;
             }
-        }
+            if(hash.find(mod) == hash.end()) {
+                    hash[mod] = i;
+            }
 
+        }
         return false;
     }
 };
