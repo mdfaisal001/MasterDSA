8551e5cc87cee822ef94e4c569acf45444c96a8e
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-11 22:15:39 +0530

    Time: 4 ms (77.11%), Space: 12 MB (48.38%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size(),maxLength = 0,count=0;
+        if(s=="") return 0;
+        if(s.empty()) return 1;
+        
+        int l=0,r=0;
+        unordered_map<char,int>mp;
+        while(r<n){
+            
+            if(mp.find(s[r]) != mp.end()){
+                l = max(l, mp[s[r]] +1);
+            }
+            mp[s[r]] = r;
+            maxLength = max(maxLength , r-l+1);
+            r++;
+        }
+        return maxLength;
+    }
+};
+/*class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size(),maxLength = INT_MIN,count=0;
+        if(s.empty()) return 1;
+        for(int i=0; i<s.size(); i++){
+            unordered_map<char,int>mp;
+            for(int j=i; j<s.size(); j++){
+                 if(mp.find(s[j]) == mp.end()){
+                    mp[s[j]] = 1;
+                 }
+                 else{
+                    maxLength = max(maxLength,j-i);
+                    break;
+                 }
+                
+            }
+        }
+        return maxLength;
+    }
+};*/
