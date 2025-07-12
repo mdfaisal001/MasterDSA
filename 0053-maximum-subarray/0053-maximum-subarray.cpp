2b23fbe616923ab549d04ef40c5a0b617ff580fe
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-12 22:52:13 +0530

    Time: 5 ms (5.47%), Space: 71.6 MB (80.69%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 2) return nums[0];
+        int sum = 0,maxLen = INT_MIN;
+        for(int i=0; i<n ;i++){
+            sum+=nums[i];
+            maxLen = max(sum,maxLen);
+            if(sum < 0) sum =0;
+            
+        }
+        return maxLen;
+    }
+};
