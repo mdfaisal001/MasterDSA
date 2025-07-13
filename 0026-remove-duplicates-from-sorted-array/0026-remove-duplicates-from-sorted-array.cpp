51f5b4e26ff85ad312bc5c3793899e9e7f4e401e
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-13 20:20:16 +0530

    Time: 0 ms (100%), Space: 22.6 MB (78.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,29 +1,14 @@
-
-/*
-    class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        set<int> st;
-        
-        for(int i=0; i<nums.size();i++){
-             st.insert(nums[i]);
-        }
-        vector<int>temp(st.begin(),st.end());
-        nums = temp;
-        return nums.size();
-    }
-}; */
-
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int j=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1]){
-                nums[j] = nums[i];
-                j++;
+        int ptr1 = 1;
+        
+        for(int i=1 ; i<nums.size(); i++){
+            if(nums[i] != nums[i-1]){
+                nums[ptr1] = nums[i];
+                ptr1++;
             }
         }
-        return j;
+        return ptr1;
     }
 };
