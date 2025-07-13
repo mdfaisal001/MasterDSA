eb04307f2d158ef7c9db438954f8402925d3b628
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-14 00:15:07 +0530

    Time: 0 ms (100%), Space: 23.7 MB (80.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-          int j =0;;
-        for(int i=0;i<nums.size(); i++){
-            if(nums[i]!=0){
-                nums[j]=nums[i];
-                j++;
+       int n = nums.size();
+       int k = 0;
+       for(int i =0; i<nums.size(); i++){
+            if(nums[i] !=0){
+                swap(nums[i],nums[k]);
+                k++;
             }
-        }
-        for(j;j<nums.size();j++){
-           nums[j] = 0;
-        }
-    }    
+       }
+    }
 };
