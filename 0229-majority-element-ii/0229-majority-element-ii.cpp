d1a69c251853ec289e7f686e01b704edd2330195
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-15 18:13:56 +0530

    Time: 0 ms (100%), Space: 23.4 MB (93.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int elem1 = 0, elem2 = 1; // must be different initial values
-        int count1 = 0, count2 = 0;
+        int elem1  =0 ,elem2 = -1;
+        int dominantElem1 = 0, dominantElem2 = 0; 
         int n = nums.size();
+        //1.traccking the two dom elem;
 
-        // Step 1: Find potential candidates
-        for (int num : nums) {
-            if (num == elem1) count1++;
-            else if (num == elem2) count2++;
-            else if (count1 == 0) {
-                elem1 = num;
-                count1 = 1;
+        // bo
+        for(int i= 0; i<n; i++){
+            if(nums[i] ==elem1) dominantElem1++;
+            else if(nums[i] == elem2) dominantElem2++;
+            else if(dominantElem1 == 0){
+                dominantElem1 = 1;
+                elem1 = nums[i];
             }
-            else if (count2 == 0) {
-                elem2 = num;
-                count2 = 1;
+            else if(dominantElem2 == 0){
+                dominantElem2 = 1;
+                elem2=nums[i];
             }
+           
             else {
-                count1--;
-                count2--;
+                dominantElem1--;
+                dominantElem2--;
             }
         }
-
-        // Step 2: Validate actual counts
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == elem1) count1++;
-            else if (num == elem2) count2++;
+        // validate its n/3;
+        int countOfDOM1  = 0, countOfDOM2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i]== elem1) countOfDOM1++;
+            else if(nums[i] == elem2) countOfDOM2++;
         }
 
-        // Step 3: Final check for > n/3
-        vector<int> result;
-        if (count1 > n / 3) result.push_back(elem1);
-        if (count2 > n / 3) result.push_back(elem2);
-        return result;
+        // final validation ;
+        vector<int>result;
+        if(countOfDOM1 > n/3){
+            result.push_back(elem1);
+        }
+        if(countOfDOM2 > n/3){
+            result.push_back(elem2);
+        }
+     return result;
     }
 };
