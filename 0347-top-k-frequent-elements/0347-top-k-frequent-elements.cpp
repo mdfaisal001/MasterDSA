bcca93bfbf6134bba5df5a144f08a881b2b7830b
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-15 11:51:47 +0530

    Time: 3 ms (51.02%), Space: 20 MB (7.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -7,6 +7,34 @@ public:
         for(int i=0; i<nums.size(); i++){
                 hashmap[nums[i]]++;
         }
+        vector<vector<int>>bucket(nums.size() +1);
+        for(auto pair : hashmap){
+            bucket[pair.second].push_back(pair.first);
+        } // added in the bucket 
+
+        for(int j = nums.size(); j>=0 ; j--){
+            //buckets have multiple elements;
+            for(auto num : bucket[j]){
+                result.push_back(num);
+                if(result.size() == k) break;
+            }
+            if(result.size() == k) break;
+        }
+        // more or  less to O(n)
+        return result;
+       // final result of most freq elements
+    }
+};
+
+/*class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>hashmap;
+        vector<int>result;
+        priority_queue<pair<int,int>> pq;
+        for(int i=0; i<nums.size(); i++){
+                hashmap[nums[i]]++;
+        }
         for(auto pair : hashmap){
             pq.push({pair.second,pair.first});
         }
@@ -18,4 +46,4 @@ public:
 
         return result;// final result of most freq elements
     }
-};
+};*/
