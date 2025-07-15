8892f4819727b16eaa075e2e29d1fd7f63225a97
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-15 12:18:14 +0530

    Time: 8 ms (13.09%), Space: 17.2 MB (5.64%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     string frequencySort(string s) {
-       map<char,int>mp;
-       for(auto c :s){
-         mp[c]++;
-       } 
-       priority_queue<pair<int,char>> pq;
-       for(auto pair : mp){
-            pq.push({pair.second,pair.first});
-       }
-       string result ="";
-       while(!pq.empty()){
-         int count = pq.top().first;
-         char word = pq.top().second;
+        // first i will count the frequency ;
+        // based on the frequency i will push the char in to bucket 
+        // will make a string from last to first thus give decreasing order;
+        unordered_map<char,int>mp;
+        int n = s.size(); //length;
+        for(int i = 0; i<n; i++){
+            mp[s[i]]++; // counting the frequency through hash map;
+        }
 
-         pq.pop();
-         while(count > 0) {
-            result+=word;
-            count--;
-         } 
-        
-       }
-       return result;
+        // push them in to the buckets;
+        vector<vector<char>> bucket(n+1); // implementing the bucket;
+        for(auto pair : mp){
+            bucket[pair.second].push_back(pair.first);
+            // pair.second represents the frequency and first rep char;
+        };
+        //output string;
+        string result = "";
+        for(int i = n ; i>=0; i--){
+            for(char letters:bucket[i]){
+                result+=string(i,letters); // forming the string based on the frequency;
+            }
+        }
+        //return the reconstructed string;
+        return result;
     }
 };
