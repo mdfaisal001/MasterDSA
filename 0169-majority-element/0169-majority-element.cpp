a58bebd6786c26f4df072844809e5a2431e2b25e
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-15 13:04:01 +0530

    Time: 0 ms (100%), Space: 28.2 MB (26.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+       int count =1;
+       int elem = nums[0];
+       for(int i=1;i<nums.size(); i++){
+            if(count == 0){
+                elem = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == elem ){
+                count++;
+            }
+            else{
+                count--;
+            }
+       }  // boyer morres algo , the maximum elements frew is not cancelled by othe el
+       
+       //2.checking that its really more than n/2;
+       int freq = 0;
+       for(int i=0; i<nums.size(); i++){
+            if(nums[i] == elem) freq++;
+       }
+       if(freq > nums.size()/2) return elem;
+       return -1;
+    }
+};
+
+
+/*class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
        unordered_map<int,int>hash;
        int size = nums.size() /2;
        //1.counting the frequency;
@@ -17,4 +46,4 @@ public:
        }
        return majorityElem;
     }
-};
+};*/
