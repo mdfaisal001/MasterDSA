8f6728fc3a5afd8b7c48262aa4ceef67f9e0633a
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-07-17 21:48:41 +0530

    Time: 3 ms (42.72%), Space: 21.6 MB (8.81%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,43 @@
 class Solution {
 private:
-    void dfs(int node,vector<int> &vis,vector<vector<int>> &adj){
-        vis[node] = 1;
-        for(int val : adj[node]){
-            if(!vis[val]){
-                dfs(val,vis,adj);
-            }
-        }   
-    }
-
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        int m = isConnected[0].size();
-        vector<int> vis(n);
-        vector<vector<int>> adj(n);
-        int province = 0;
-        for(int row = 0; row<n; row++){
-            for(int col = 0; col<m; col++ ){
-                if(isConnected[row][col]==1){
-                    adj[row].push_back(col);
-                    adj[col].push_back(row);
+   void bfs(int node,vector<int>&vis , vector<vector<int>> &adj){
+        vis[node]=1;
+        queue<int>que;
+        que.push(node);
+        while(!que.empty()){
+            int insertedNode = que.front();
+            que.pop();
+            for(int it :adj[insertedNode]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    que.push(it);
                 }
             }
         }
-        for(int i=0;i<n;i++){
+   }
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size(); //row;
+        int m = isConnected[0].size(); // col;
+       // vis arr, province,bfs,adjlist
+       vector<int>vis(n,0); //0 based
+       int province = 0;
+       vector<vector<int>> adj(n);//size is must
+       for(int row=0; row<m; row++){
+           for(int col=0; col<m ;col++){
+                if(isConnected[row][col] == 1){
+                    adj[row].push_back(col);
+                    adj[col].push_back(row);
+                }
+           }
+       }
+       for(int i=0; i<n; i++){
             if(!vis[i]){
-                province++;
-                dfs(i,vis,adj);
+                province++; //sep comp++;
+                bfs(i,vis,adj);
             }
-        }
-        return province;
+       }
+       return province;
     }
 };
