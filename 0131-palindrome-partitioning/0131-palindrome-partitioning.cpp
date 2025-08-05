cf04328250609f721e0568d9483ae54694990595
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-08-05 21:49:54 +0530

    Time: 128 ms (14.2%), Space: 145 MB (10.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,30 @@
 class Solution {
 private:
-    bool findPalindrome(string &substring , int start , int end){
-        while(start < end){
-            if(substring[start]!=substring[end]){
-                return false;
-            }
-            start++;
-            end--;
+    bool isPalindrome(int low , string &s , int high){
+        while(low < high){
+             if(s[low++] != s[high--]) return false;
         }
         return true;
     }
-    void findPartition(int start, string &s, vector<string> temp, vector<vector<string>> &result){
-        if(start == s.size()){
-            result.push_back(temp);
+  void findPartition(int index,string s , vector<vector<string>> &result,vector<string> container){
+        if(index == s.size()){
+            result.push_back(container);
             return;
         }
 
-        for(int end=start; end<s.size(); end++){
-            if(findPalindrome(s,start,end)){
-                temp.push_back(s.substr(start, end-start +1));
-                findPartition(end+1,s,temp,result);
-                temp.pop_back();
+        for(int i=index;i<s.size(); i++){
+            if(isPalindrome(index,s,i)){
+                container.push_back(s.substr(index,i-index+1));
+                findPartition(i+1,s,result,container);
+                container.pop_back();
             }
         }
-    }
+  }
 public:
     vector<vector<string>> partition(string s) {
-        vector<string> temp;
-        vector<vector<string>>result;
-        int start = 0;
-        findPartition(start,s,temp,result);
-        return result;
+       vector<vector<string>> result;
+       vector<string> container;
+       findPartition(0,s,result,container);
+       return result;
     }
 };
