a42d74618fb5fa6cd69fb01e4930151c90054d5d
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-08-15 23:36:01 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.73%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,31 @@
 class Solution {
-    
-private:
-        unordered_map<char, string> mp= {
+         unordered_map<char, string> mp= {
                 {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
     };
+    private:
+        void findCombination(int index, string &temp, vector<string> &result, string &digits){
+            if(index == digits.size()){
+                result.push_back(temp);
+                return;
+            }
 
+            string currentString = mp[digits[index]];
+            for(int i=0; i<currentString.size() ; i++){
+                temp.push_back(currentString[i]);
+                findCombination(index+1,temp,result,digits);
+                temp.pop_back();
+            }
+        }
 
-    void findCombination(int index , string combination, vector<string> &result, string &digits){
-        if(index == digits.size()){
-            result.push_back(combination);
-        }
-        
-        string temp = mp[digits[index]];
-        for(char ch : temp){
-            combination.push_back(ch);
-            findCombination(index+1,combination,result,digits);
-            combination.pop_back();
-        }
-}
+    
+private:
 
 public:
     vector<string> letterCombinations(string digits) {
+        string temp = "";
         if(digits.empty()) return {};
-       string combination ="";
-       vector<string> result;
-       findCombination(0,combination,result,digits);
-       return result;
+        vector<string> result;
+        findCombination(0,temp,result,digits);
+        return result;
     }
 };
