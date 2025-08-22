1a8ea52d57f8de49933cfa5ea752f804d30ef622
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-08-22 20:44:59 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int point = -1;
+        int n = nums.size();
+        for(int i=n-2 ; i>=0 ; i--){
+            if(nums[i] < nums[i+1]){
+                point = i;
+                break;
+            }
+        } // find the satisfying point;
+
+        //if already in lexigrophically descending oreder reverse the perm;
+        if(point == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+       
+        for(int i= n-1; i>point; i--){
+             if(nums[i] > nums[point]){
+                swap(nums[i] , nums[point]);
+                break;
+             }
+        }
+
+        reverse(nums.begin() + point +1 , nums.end());
     }
 };
