9140a1504d6c042704e01e583256c280806be9f2
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-09-05 06:08:34 +0530

    Time: 75 ms (73.28%), Space: 146.3 MB (55.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>hash;
-        hash[0] = -1;
-        int n = nums.size(), sum = 0;int mod = -1;
-        for(int i=0; i<n ; i++){
-            sum+=nums[i];
-            mod = (k==0)? sum : sum%k; 
-            if(hash.find(mod) != hash.end()){
-                int len = i - hash[mod];
-                if(len >=2) return true;
+        unordered_map<int,int>firstOccur;
+        int n=nums.size(), prefix = 0;
+        firstOccur[0] = -1;
+        for(int i=0; i<n;i++){
+            prefix+=nums[i];
+            if(firstOccur.find(prefix%k) != firstOccur.end()){
+                 if(i-firstOccur[prefix%k] >= 2) return true;
             }
-            if(hash.find(mod) == hash.end()) {
-                    hash[mod] = i;
+            else{
+                firstOccur[prefix % k] = i;
             }
+      }
+       return false;
 
-        }
-        return false;
-    }
+    }      
+    
 };
