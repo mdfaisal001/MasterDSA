2a71859352790fcf120fcf350daae77e876765b0
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-09-29 19:52:37 +0530

    Time: 0 ms (100%), Space: 27.3 MB (30.84%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,33 @@
+class Solution {
+
+public:
+    int trap(vector<int>& height) {
+
+        //to find maximum height from l to r;
+        int n = height.size();
+        vector<int> maxLeftHeight(n);
+        maxLeftHeight[0] = height[0];
+        for(int i=1; i<height.size(); i++){
+            maxLeftHeight[i] = max(maxLeftHeight[i-1],height[i]);
+        }
+
+        // to find maximum height from r to l;
+
+        vector<int> maxRightHeight(n);
+        maxRightHeight[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            maxRightHeight[i] = max(maxRightHeight[i+1], height[i]);
+        }
+
+
+        // now find the water which is stored between two walls;
+        int total = 0;
+        for(int i=0; i<n;i++){
+            if(maxLeftHeight[i] > height[i] && height[i] < maxRightHeight[i]){
+                total += min(maxLeftHeight[i],maxRightHeight[i]) - height[i];
+            }
+        }
+
+        return total;
+    }
+};
