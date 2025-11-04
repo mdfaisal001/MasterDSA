cc73883f512b0f0f77cee71fc3e7e5505cc4c254
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-11-04 07:02:27 +0530

    Time: 58 ms (22.3%), Space: 8.2 MB (10.94%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,19 +1,12 @@
 class Solution {
+    vector<int> dp; 
 public:
     int fib(int n) {
-        if(n == 0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        dp.resize(n+1,-1);
+        if(n<=1) return n;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = fib(n-1) + fib(n-2);
     }
 };
-
-/*   int fib_0 = 0;
-        int fib_1 = 1;
-        if (n<=1) return n;
-        int currentFib;
-        for(int i=2;i<=n; i++){
-            currentFib = fib_0 + fib_1;
-            fib_0 = fib_1;
-            fib_1 = currentFib;
-        }
-        return currentFib;*/
