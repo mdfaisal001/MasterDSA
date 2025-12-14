6350e85aaf778871d134bbf827d7b67919abf4b4
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-12-14 23:42:56 +0530

    Time: 5 ms (10.94%), Space: 9.4 MB (27.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char>mp;
-          map<char,char>mp2;
+        if(s.size() != t.size()) return false;
+        unordered_map<char,char> mp1;
+         unordered_map<char,char> mp2;
         for(int i=0; i<s.size(); i++){
-            char c1 = s[i] , c2 = t[i];
-            if(mp.count(c1) && mp[c1]!=c2) return false;
-            if(mp2.count(c2) && mp2[c2] != c1) return false;
-            mp[c1] = c2;
-            mp2[c2] = c1;
+           if(mp1.count(s[i]) && mp1[s[i]] != t[i]) return false;
+           if(mp2.count(t[i]) && mp2[t[i]] != s[i]) return false;
+           mp1[s[i]] = t[i];
+           mp2[t[i]] = s[i];
+
         }
         return true;
     }
 };
 
-/*if(s.size()!= t.size()) return false;
-        for(int i= 0; i<s.size(); i++){
-            char c1 = s[i];
-           if(map.count(c1) == 0)
-        }
-        string res ="";
+/*class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> mp1;
+         unordered_map<char,int> mp2;
         for(int i=0; i<s.size(); i++){
-           res+=mp[s[i]]; 
+            mp1[s[i]]++;
+            mp2[t[i]]++;
         }
-        if(res == t) return true;
-        return false;*/
+
+        for(int i =0; i<s.size(); i++){
+            if(mp1[s[i]] != mp2[t[i]]) return false;
+        }
+
+        return true;
+    }
+};*/
