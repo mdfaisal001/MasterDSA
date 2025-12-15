7ca40f10a503205842093357a84275f3e6485c24
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2025-12-15 15:19:23 +0530

    Time: 11 ms (28.64%), Space: 14.4 MB (18.69%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        st.push(s[0]);
+        for(int i=1; i<s.size(); i++){
+            if(!st.empty() && st.top() == s[i]){
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+        string result;
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
