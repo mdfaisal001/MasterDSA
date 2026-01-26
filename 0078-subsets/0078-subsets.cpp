44417a0efe50b350bbe2ca5c2a5f5f2d266a4ff7
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-01-26 23:23:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (95.4%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 private:
-    void findSubset(int index , vector<vector<int>> &result,vector<int> &temp, vector<int> &nums){
-        int n = nums.size();
-        if(index == n){
-            result.push_back(temp);
-            return;
-        } // base case for printing the subset;
-
-        temp.push_back(nums[index]); // picking the case
-        findSubset(index+1,result,temp,nums); // call the pick 
-        temp.pop_back(); // not picking 
-        findSubset(index+1, result,temp,nums); // call the not pick;
+void findSubsets(int index,vector<int>&nums,vector<int> &temp,vector<vector<int>> &result){
+    if(index == nums.size()){
+        result.push_back(temp);
+        return;
     }
+
+    temp.push_back(nums[index]);
+    findSubsets(index+1,nums,temp,result);
+    temp.pop_back();
+    findSubsets(index+1,nums,temp,result);
+}
 
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> temp;
-        findSubset(0,result,temp,nums);
-        return result;
+      vector<vector<int>> result;
+      vector<int> temp;
+      findSubsets(0,nums,temp,result);
+      return result;
     }
 };
