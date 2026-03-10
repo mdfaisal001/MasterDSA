af8bb58e52bf5bac3e8e042f35b03322980932ee
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-03-10 22:47:07 +0530

    Time: 3 ms (47.05%), Space: 10.1 MB (10.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+         unordered_map<char,int> mp;
+         for(auto ch : s){
+             mp[ch]++;
+         }
+
+         for(auto it : t){
+             if(mp.find(it) == mp.end()) return false;
+
+             mp[it]--;
+             if(mp[it] == 0) mp.erase(it);
+         }
+        return mp.empty();
+        
+       }
+       
+    
+};
+
+/*class Solution {
+public:
+    bool isAnagram(string s, string t) {
         int freq[26] = {0};
         if (s.length() != t.length())
             return false;
@@ -15,4 +37,4 @@ public:
        }
        
     
-};
+};*/
