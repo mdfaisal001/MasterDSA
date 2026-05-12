35c149bc9f16507ff5de35bf4186820f9eab8e5d
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-05-12 06:47:37 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,29 +3,29 @@ class Solution {
                 {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
     };
     private:
-        void findCombination(int index, string &temp, vector<string> &result, string &digits){
-            if(index == digits.size()){
-                result.push_back(temp);
-                return;
-            }
-
-            string currentString = mp[digits[index]];
-            for(int i=0; i<currentString.size() ; i++){
-                temp.push_back(currentString[i]);
-                findCombination(index+1,temp,result,digits);
-                temp.pop_back();
-            }
+     void findLetterCombination(int index, string &temp, vector<string> &result,string digits){
+        if(index == digits.size()){
+            result.push_back(temp);
+            return;
         }
 
-    
-private:
+        string currString = mp[digits[index]];
+
+        for(char c : currString){
+            temp.push_back(c);
+            findLetterCombination(index+1,temp,result,digits);
+            temp.pop_back();
+        }
+     }
+
 
 public:
     vector<string> letterCombinations(string digits) {
-        string temp = "";
-        if(digits.empty()) return {};
+        string temp="";
+        if(digits.size() == 0) return {};
         vector<string> result;
-        findCombination(0,temp,result,digits);
+
+        findLetterCombination(0,temp,result,digits);
         return result;
     }
 };
