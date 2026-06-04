bf5e94283fe927c9cd4ec4bf09070f2243df35d0
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-06-04 16:25:34 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.8%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int  n = matrix.size();
+
+        //to traverse one part above the 
+        for(int i=0;i<n-1;i++){
+
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+
+        for(int i=0;i<matrix.size(); i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }  
+};
+
+
+/*class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int  n = matrix.size();
         vector<vector<int>> rotated(n,vector(n,0));
         for(int row =0; row < n; row++){
             for(int col=0; col< matrix[0].size(); col++){
@@ -14,5 +35,5 @@ public:
                 matrix[i][j] = rotated[i][j];
             }
         }
-    }
-};
+    }  //brute solution   
+};*/
