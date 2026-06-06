f8a9ced2b4815e3ccbe4d451d4365ee5023cf713
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-06-06 06:31:08 +0530

    Time: 0 ms (100%), Space: 23.7 MB (44.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int elem1  =0 ,elem2 = -1;
-        int dominantElem1 = 0, dominantElem2 = 0; 
-        int n = nums.size();
-        //1.traccking the two dom elem;
+       int elem1 = 0, elem2 = 0;
+       int freq1 = 0, freq2= 0;
+       int n = nums.size();
 
-        // bo
-        for(int i= 0; i<n; i++){
-            if(nums[i] ==elem1) dominantElem1++;
-            else if(nums[i] == elem2) dominantElem2++;
-            else if(dominantElem1 == 0){
-                dominantElem1 = 1;
+       for(int i=0; i<n; i++){
+
+            if(nums[i] == elem1 ) freq1++;
+            else if(nums[i] == elem2) freq2++;
+            else if(freq1 == 0){
+                freq1=1;
                 elem1 = nums[i];
             }
-            else if(dominantElem2 == 0){
-                dominantElem2 = 1;
-                elem2=nums[i];
+            else if(freq2 == 0){
+                freq2 = 1;
+                elem2 = nums[i];
             }
-           
             else {
-                dominantElem1--;
-                dominantElem2--;
+                freq1--;
+                freq2--;
             }
-        }
-        // validate its n/3;
-        int countOfDOM1  = 0, countOfDOM2 = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i]== elem1) countOfDOM1++;
-            else if(nums[i] == elem2) countOfDOM2++;
-        }
 
-        // final validation ;
-        vector<int>result;
-        if(countOfDOM1 > n/3){
-            result.push_back(elem1);
-        }
-        if(countOfDOM2 > n/3){
-            result.push_back(elem2);
-        }
-     return result;
+       }
+
+       // checking the frequency of maj elem greater than the n/3
+
+       int currFreq1 = 0, currFreq2 = 0;
+
+       for(int i=0;i<nums.size(); i++){
+           if(nums[i] == elem1) currFreq1++;
+           else if(nums[i] == elem2) currFreq2++; 
+       }
+    
+      //For result 
+      vector<int>result;
+       if(currFreq1 > n/3) result.push_back(elem1);
+       if (currFreq2 > n/3) result.push_back(elem2); 
+       return result;
     }
+
+    
 };
