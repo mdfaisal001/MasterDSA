f015e228ba9bd0ad9f6fb0ec6f6f8e429c60a4e6
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-07-12 07:18:35 +0530

    Time: 3 ms (35.09%), Space: 11.3 MB (19.04%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,18 +3,20 @@ public:
     string reverseWords(string s) {
        stringstream ss(s);
        string word;
-       vector<string>words;
+       vector<string> words;
        while(ss >> word){
-          words.push_back(word);
+            words.push_back(word);
        }
-       reverse(words.begin(), words.end());
-       string finalString = "";
-       for(int i=0; i<words.size(); i++){
-            finalString+=words[i];
-            if(i!=words.size()-1){
-                finalString+=" ";
-            }
+
+       string result="";
+       reverse(words.begin(),words.end());
+       for(int i=0;i<words.size();i++){
+           
+            result+=words[i];
+
+            if(i < words.size()-1) result+=" ";
        }
-       return finalString;
+
+       return result;
     }
 };
