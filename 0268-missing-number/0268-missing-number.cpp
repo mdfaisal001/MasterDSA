9611a2e81d8240838d3e3d6674fba2891b50872c
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2026-07-16 10:16:54 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.96%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int total =0;
+        for(int i=1;i<=nums.size(); i++){
+            total+=i;
+        }
+
+        int sum =0;
+        for(int i=0;i<nums.size(); i++){
+            sum+=nums[i];
+        }
+
+        return total-sum;
+
+    }
+};
